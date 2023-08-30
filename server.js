bd18b6f99615681eaf4f3aa2bff0cad3f5ab3684
federico almaraz <federicoalmaraz37@gmail.com>
2023-08-30 18:29:23 -0300

    add .js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from "express"
-import handlers from "./src/routes/index"
-import { createPrismaClient } from "./src/config/db";
-import { errorHandler } from "./src/middleware/error-handlers";
+// import handlers from "./src/routes/index"
+import handlers from "./src/routes/index.ts"
+import { createPrismaClient } from "./src/config/db.js";
+import { errorHandler } from "./src/middleware/error-handlers.js";
 
 
 const server = express()
