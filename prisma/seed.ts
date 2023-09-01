a3c080ddce7416d9557baec7586cc9d95268cafe
federico almaraz <federicoalmaraz37@gmail.com>
2023-08-31 22:16:23 -0300

    seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,72 +1,36 @@
 import { PrismaClient } from "@prisma/client";
-import { products } from "./products.ts";
+import { v4 as uuid } from "uuid";
 
 const prisma = new PrismaClient();
 
 export async function seed() {
-  // await prisma.states.createMany({
-  //   data: [
-  //     {
-  //       state: 'active',
-  //     },
-  //     {
-  //       state: 'pending',
-  //     },
-  //     {
-  //       state: 'cancelled',
-  //     },
-  //     {
-  //       state: 'disabled',
-  //     },
-  //     {
-  //       state: 'approved',
-  //     },
-  //     {
-  //       state: 'rejected',
-  //     },
-
-  //   ]
-  // })
+  await prisma.category.createMany({
+    data: [
+      {
+        category: 'Indumentaria Masculina',
+      },
+      {
+        category: 'Indumentaria Femenina',
+      },
+      {
+        category: 'Decoracion',
+      },
+      {
+        category: 'Accesorios',
+      },
+      {
+        category: 'Juguetes',
+      },
+    ]
+  })
   //   await prisma.role.createMany({
   //       data:[
   //           {role: 'admin'},
   //           {role: 'user' }
   //       ]
   //   });
-    // await prisma.products.createMany({
-    //   data:[
-    //     {
-    //       name: 'Camiseta Titular AFA',
-    //       imgUrl: 'ta-seleccion-hombre.jpg?raw=true',
-    //       description: '1 producir contenido virgen.',
-    //       price: 23000,
-    //       brand: "Adidas" ,
-    //       categoryId: 1,
-    //     },
-    //     {
-    //       name: 'Camiseta Copa America',
-    //       imgUrl: 'ic/rica-3.jpg?raw=true',
-    //       description: 'r reciclado, 49 % poliéster.',
-    //       price: 20000,
-    //       brand: "Adidas" ,
-    //       categoryId: 1,
-    //     },
-    //     {
-    //       name: '¡La del Dibu!',
-    //       imgUrl: 'ublig?raw=true',
-    //       description: 'yn las ju mayor tensión.',
-    //       price: 15000,
-    //       brand: "Adidas" ,
-    //       categoryId: 1,
-    //     },
-    //   ]
-    // })
-    for (let product of products){
-      await prisma.products.createMany({
-        data:  product
-      })
-      
-    }
+    
+
 }
 
 seed()
