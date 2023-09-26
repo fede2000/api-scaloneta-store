7b9127c56fa511639bdae6659f93c0a68501094d
federico almaraz <federicoalmaraz37@gmail.com>
2023-09-26 10:41:46 -0300

    correcion 2

--- server.ts
+++ server.ts
@@ -13,7 +13,7 @@ server.use(express.json())
 
 //cors enable
 server.use((req,res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     res.header('Access-Control-Allow-Headers', 'Content-Type')
     next()
